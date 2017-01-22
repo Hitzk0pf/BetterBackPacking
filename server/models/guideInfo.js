27d9e9e1f7ae4865870701860dfb97f1dfb20821
Thomas Weger <Thomas@Thomass-MBP.home>
2017-01-22 21:08:04 +0100

    created guideInfo table

---
+++ server/models/guideInfo.js
@@ -0,0 +1,50 @@
+var bcrypt = require('bcrypt');
+var cuid = require('cuid');
+
+const GuideInfoModel = (sequelize, Sequelize) => {
+  const GuideInfo = sequelize.define('guideInfo', {
+        cuid: {
+                type: Sequelize.STRING,
+                primaryKey: true,
+                validate: {
+                        notEmpty: true,
+                }
+        },
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		}
+	},
+	characterTraits: {
+		type: Sequelize.ARRAY,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		}
+	},
+	characterTraitDescription: {
+		type: Sequelize.ARRAY,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		}
+	}
+  }, {
+	tableName: 'guideInfos',
+	freezeTableName: true,
+  });
+
+  /*GuideInfo.sync({force: true}).then(function () {
+    // Testtour at serverstart
+    return Tour.create({
+      cuid: cuid(),
+      description: 'I am so and so old...',
+    });
+  });*/
+
+  return Tour;
+}
+
+export default GuideInfoModel;
