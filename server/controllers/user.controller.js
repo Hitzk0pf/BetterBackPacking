d30551eba0f4ffc00628b6c5a60aa7069e50a6df
raphi98 <rjofner@gmx.at>
2017-01-23 11:33:17 +0100

    added Authentication to deleteUser

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -178,13 +178,25 @@ export function changeUser(req, res) {
     }
 
 
+export function deleteUser(req, res) {
 
+  models.User.findOne({ where: {cuid: req.params.cuid} }).then((user) => {
+    if(user) {
+      user.destroy();
+      res.status(200).end();
+    } else {
+      res.status(404).send();
+    }
+  }).catch(err => res.status(500).send(err));
+
+}
 /**
  * Delete a User
  * @param req
  * @param res
  * @returns void
  */
+ /*
 export function deleteUser(req, res) {
   models.User.findOne({ cuid: req.params.cuid }).exec((err, User) => {
     if (err) {
@@ -195,4 +207,4 @@ export function deleteUser(req, res) {
       res.status(200).end();
     });
   });
-}
+}*/
